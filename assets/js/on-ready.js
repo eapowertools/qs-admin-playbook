f76ccad6b4b483a133889455d023df4604240ebf
danielpilla <daniel.b.pilla@gmail.com>
2020-04-28 09:59:42 -0400

    Update on-ready.js

--- assets/js/on-ready.js
+++ assets/js/on-ready.js
@@ -386,4 +386,15 @@ $(document).ready(function() {
 
         return;
     }
+    
+    // nav labels
+    $('.navigation-list-child-list').each(function() {
+        $($(this)[0].childNodes).each(function() {
+            var itemCount = 0;
+            try {
+                var itemCount = $(this)[0].childNodes[1].childElementCount;
+                $(this)[0].innerHTML = $(this)[0].innerHTML.replace($(this)[0].innerText, $(this)[0].innerText + ' (' + itemCount + ')');
+            } catch (e) {}
+        })
+    })
 });
