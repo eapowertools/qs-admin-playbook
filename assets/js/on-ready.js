53a0349215045e747db2aed866776d31522d8859
danielpilla <daniel.b.pilla@gmail.com>
2020-04-28 13:57:16 -0400

    Update on-ready.js

--- assets/js/on-ready.js
+++ assets/js/on-ready.js
@@ -1,27 +1,97 @@
 $(document).ready(function() {
-    
-    function Check_Version(){
-        var rv = -1; // Return value assumes failure.
-
-        if (navigator.appName == 'Microsoft Internet Explorer'){
-
-           var ua = navigator.userAgent,
-               re  = new RegExp("MSIE ([0-9]{1,}[\\.0-9]{0,})");
-
-           if (re.exec(ua) !== null){
-             rv = parseFloat( RegExp.$1 );
-           }
+        if (!Array.prototype.includes) {
+            Object.defineProperty(Array.prototype, "includes", {
+                enumerable: false,
+                value: function(obj) {
+                    var newArr = this.filter(function(el) {
+                        return el == obj;
+                    });
+                    return newArr.length > 0;
+                }
+            });
         }
-        else if(navigator.appName == "Netscape"){                       
-           /// in IE 11 the navigator.appVersion says 'trident'
-           /// in Edge the navigator.appVersion does not say trident
-           if(navigator.appVersion.indexOf('Trident') === -1) rv = 12;
-           else rv = 11;
-        }       
 
-        return rv;          
-    }
+        if (!Array.from) {
+            Array.from = (function() {
+                var toStr = Object.prototype.toString;
+                var isCallable = function(fn) {
+                    return typeof fn === 'function' || toStr.call(fn) === '[object Function]';
+                };
+                var toInteger = function(value) {
+                    var number = Number(value);
+                    if (isNaN(number)) {
+                        return 0;
+                    }
+                    if (number === 0 || !isFinite(number)) {
+                        return number;
+                    }
+                    return (number > 0 ? 1 : -1) * Math.floor(Math.abs(number));
+                };
+                var maxSafeInteger = Math.pow(2, 53) - 1;
+                var toLength = function(value) {
+                    var len = toInteger(value);
+                    return Math.min(Math.max(len, 0), maxSafeInteger);
+                };
 
+                // The length property of the from method is 1.
+                return function from(arrayLike /*, mapFn, thisArg */ ) {
+                    // 1. Let C be the this value.
+                    var C = this;
+
+                    // 2. Let items be ToObject(arrayLike).
+                    var items = Object(arrayLike);
+
+                    // 3. ReturnIfAbrupt(items).
+                    if (arrayLike == null) {
+                        throw new TypeError("Array.from requires an array-like object - not null or undefined");
+                    }
+
+                    // 4. If mapfn is undefined, then let mapping be false.
+                    var mapFn = arguments.length > 1 ? arguments[1] : void undefined;
+                    var T;
+                    if (typeof mapFn !== 'undefined') {
+                        // 5. else
+                        // 5. a If IsCallable(mapfn) is false, throw a TypeError exception.
+                        if (!isCallable(mapFn)) {
+                            throw new TypeError('Array.from: when provided, the second argument must be a function');
+                        }
+
+                        // 5. b. If thisArg was supplied, let T be thisArg; else let T be undefined.
+                        if (arguments.length > 2) {
+                            T = arguments[2];
+                        }
+                    }
+
+                    // 10. Let lenValue be Get(items, "length").
+                    // 11. Let len be ToLength(lenValue).
+                    var len = toLength(items.length);
+
+                    // 13. If IsConstructor(C) is true, then
+                    // 13. a. Let A be the result of calling the [[Construct]] internal method of C with an argument list containing the single item len.
+                    // 14. a. Else, Let A be ArrayCreate(len).
+                    var A = isCallable(C) ? Object(new C(len)) : new Array(len);
+
+                    // 16. Let k be 0.
+                    var k = 0;
+                    // 17. Repeat, while k < len… (also steps a - h)
+                    var kValue;
+                    while (k < len) {
+                        kValue = items[k];
+                        if (mapFn) {
+                            A[k] = typeof T === 'undefined' ? mapFn(kValue, k) : mapFn.call(T, kValue, k);
+                        } else {
+                            A[k] = kValue;
+                        }
+                        k += 1;
+                    }
+                    // 18. Let putStatus be Put(A, "length", len, true).
+                    A.length = len;
+                    // 20. Return A.
+                    return A;
+                };
+            }());
+        }
+    
     try {
         // initialize select pickers
         $(".selectpicker").selectpicker();
@@ -152,25 +222,6 @@ $(document).ready(function() {
             }
             row[1] = newArray;
         });
-        
-//         // This is the hack for IE
-//         if (Check_Version()=='11') {
-//           $(".selectpicker").click(function() {
-              
-//             categoryFilterSelections = $('#categoryFilter').val();
-//             toolingFilterSelections = $('#toolingFilter').val();
-
-//             reRenderPlaybook();
-//           });
-//         }
-        
-        $(".selectpicker").bind(Check_Version()=='11'? 'propertychange': 'change', function(e) {
-            e.preventDefault();
-            categoryFilterSelections = $('#categoryFilter').val();
-            toolingFilterSelections = $('#toolingFilter').val();
-
-            reRenderPlaybook();
-        }); 
 
         // re-render the playbook on change of filters
         $('.selectpicker').on('change', function() {
@@ -449,7 +500,7 @@ $(document).ready(function() {
             return;
         }
     } catch (e) {
-
+        console.log(e);
     }
 
     // nav labels
